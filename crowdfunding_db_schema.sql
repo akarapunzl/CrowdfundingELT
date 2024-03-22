2a20874e03d9b0e1b073df3fc51e6fbe245c4ccb
akarapunzl <Samanthagduffy@gmail.com>
2024-03-21 22:07:28 -0500

    final code updates

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,32 +1,32 @@
 CREATE TABLE categories(
-category_id VARCHAR(30) PRIMARY KEY,
-category VARCHAR(30));
+category_id VARCHAR(30) NOT NULL PRIMARY KEY,
+category VARCHAR(30) NOT NULL);
 
 CREATE TABLE subcategories(
-subcategory_id VARCHAR(30) PRIMARY KEY,
-subcategory VARCHAR(30));
+subcategory_id VARCHAR(30) NOT NULL PRIMARY KEY,
+subcategory VARCHAR(30) NOT NULL);
 
 CREATE TABLE contacts(
-contact_id INT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-email VARCHAR(50));
+contact_id INT NOT NULL PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+email VARCHAR(50) NOT NULL);
 
 CREATE TABLE campaign(
-cf_id INT PRIMARY KEY,
-contact_id INT,
-company_name VARCHAR(100),
-description VARCHAR(100),
-goal FLOAT(10),
-pledged FLOAT(10),
-outcome VARCHAR(30),
-backers_count INT,
-country VARCHAR(30),
-currenty VARCHAR(30),
-launch_date DATE,
-end_date DATE,
-category_id VARCHAR(30),
-subcategory_id VARCHAR(30),
+cf_id INT NOT NULL PRIMARY KEY,
+contact_id INT NOT NULL,
+company_name VARCHAR(100) NOT NULL,
+description VARCHAR(100) NOT NULL,
+goal FLOAT NOT NULL ,
+pledged FLOAT NOT NULL,
+outcome VARCHAR(30) NOT NULL,
+backers_count INT NOT NULL,
+country VARCHAR(30) NOT NULL,
+currenty VARCHAR(30) NOT NULL,
+launch_date DATE NOT NULL,
+end_date DATE NOT NULL,
+category_id VARCHAR(30) NOT NULL,
+subcategory_id VARCHAR(30) NOT NULL,
 FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
 FOREIGN KEY (category_id) REFERENCES categories(category_id),
 FOREIGN KEY (subcategory_id) REFERENCES subcategories(subcategory_id));
